c6fbfaeb8d004364a4a68e582d54f12e7bdd99e9
Maheswari Kumar <ksrimahi@gmail.com>
2023-09-14 21:11:11 +0530

    App

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,8 +3,18 @@ import React from "react";
 function Main(){
     return (
         <div className="Main-section">
+            <div className="categories"></div>
+            <FeaturedMusic />
         </div>
 
+    )
+}
+
+function FeaturedMusic(){
+    return (
+        <div className="feature">
+            <h1>Featured This Week</h1>
+        </div>
     )
 }
 
