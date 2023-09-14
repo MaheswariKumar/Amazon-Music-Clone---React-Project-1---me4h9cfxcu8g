335a97ddc204cb2fe0e03baf3e78dd3dbeefb449
Maheswari Kumar <ksrimahi@gmail.com>
2023-09-14 22:45:32 +0530

    Done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,7 +11,7 @@ function NavBar(){
         <div id="Navbar">
             <div className="left-icons">
                 <ul className="left-lists">
-                    <li><img className="logo" src="https://d5fx445wy2wpk.cloudfront.net/static/logo.svg"></img></li>
+                    <div><img className="logo" src="https://d5fx445wy2wpk.cloudfront.net/static/logo.svg"></img></div>
                     <li><HomeIcon color="inherit"/></li>
                     <li>HOME</li>
                     <li><PodcastIcon color="inherit"/></li>
@@ -24,10 +24,12 @@ function NavBar(){
             <div className="right-icons">
                 <ul className="right-lists">
                     <form>
-                        <input className="search" type="search" placeholder="Search"></input>
-                        <SearchIcon color="inherit"/>
+                        <div className="search-container">
+                            <input className="search" type="search" placeholder="Search"></input>
+                            <SearchIcon color="inherit"/>
+                        </div>
                     </form>
-                    <li><ProfileIcon color="inherit"/></li>
+                    <li className="user-icon"><ProfileIcon color="inherit"/></li>
                 </ul>
             </div>
         </div>
