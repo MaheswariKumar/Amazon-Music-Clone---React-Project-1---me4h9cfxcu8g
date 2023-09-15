f809493804dcbe39734ff15725cc5089090daea9
Maheswari Kumar <ksrimahi@gmail.com>
2023-09-15 23:08:09 +0530

    Done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,25 +11,36 @@ function NavBar(){
         <div id="Navbar">
             <div className="left-icons">
                 <ul className="left-lists">
-                    <div><img className="logo" src="https://d5fx445wy2wpk.cloudfront.net/static/logo.svg"></img></div>
-                    <li><HomeIcon color="inherit"/></li>
-                    <li>HOME</li>
-                    <li><PodcastIcon color="inherit"/></li>
-                    <li>PODCASTS</li>
-                    <li><MyMusicIcon color="inherit"/></li>
-                    <li>LIBRARY</li>
-                    <li><ChevronCaretdownIcon color="inherit"/></li>
+                    <div className="logo-div">
+                        <img className="logo" src="https://d5fx445wy2wpk.cloudfront.net/static/logo.svg"></img>
+                    </div>
+                    <div className="home-div">
+                        <li><HomeIcon color="inherit"/></li>
+                        <li>HOME</li>
+                    </div>
+                    <div className="pod-div">
+                        <li><PodcastIcon color="inherit"/></li>
+                        <li>PODCASTS</li>
+                    </div>
+                    <div className="lib-div">
+                        <li><MyMusicIcon color="inherit"/></li>
+                        <li>LIBRARY</li>
+                        <li><ChevronCaretdownIcon color="inherit"/></li>
+                    </div>
                 </ul>
             </div>
             <div className="right-icons">
                 <ul className="right-lists">
                     <form>
                         <div className="search-container">
+                            <span class="fa fa-search"></span>
                             <input className="search" type="search" placeholder="Search"></input>
-                            <SearchIcon color="inherit"/>
+                            {/* <i><SearchIcon color="inherit"/></i> */}
                         </div>
                     </form>
-                    <li className="user-icon"><ProfileIcon color="inherit"/></li>
+                    <div className="user-icon">
+                        <li><ProfileIcon color="inherit"/></li>
+                    </div>
                 </ul>
             </div>
         </div>
