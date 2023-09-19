71d62f4126fe05f5cea350cb94f22175c5c4701b
Maheswari Kumar <ksrimahi@gmail.com>
2023-09-19 23:24:16 +0530

    Done

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import ChevronCaretLeftIcon from "./ChevronCaretLeftIcon";
+import ChevronCaretrightIcon from "./ChevronCaretrightIcon";
 
 function Main(){
+    const [featuredata, setFeaturedata] = useState([]);
+
     useEffect(()=>{
         async function fetchFeaturedMusic() {
             try {
-                const response = await fetch('https://academics.newtonschool.co/api/v1/music/song', {
+                const response = await fetch('https://academics.newtonschool.co/api/v1/music/album', {
                     headers: {
                         'projectId': 'f104bi07c490' // Replace with your actual project ID or API key
                     }
@@ -18,6 +22,11 @@ function Main(){
         
                 // Process the data here
                 console.log(data); // You can log the data or perform further actions
+                setFeaturedata(data);
+                console.log(data.data);
+                console.log(data.data[0]);
+                const popularAlbums = data.filter(album => album.category === 'Popular');
+                console.log(popularAlbums);
         
             } catch (error) {
                 console.error('There was a problem with the fetch operation:', error);
@@ -32,6 +41,7 @@ function Main(){
         <div className="Main-section">
             <div className="categories"></div>
             <FeaturedMusic />
+            <SoulSoothers />
         </div>
 
     )
@@ -40,9 +50,97 @@ function Main(){
 function FeaturedMusic(){
     return (
         <div className="feature">
-            <h1>Featured This Week</h1>
+            <div className="headertab">
+                <div className="header">
+                    <h2>Featured This Week</h2>
+                </div>
+                <div className="options">
+                    <div>
+                        <ChevronCaretLeftIcon style={{ color: 'white' }}/>
+                    </div>
+                    <div>
+                        <ChevronCaretrightIcon style={{ color: 'white' }}/>
+                    </div>
+                </div>
+                <div className="alloptions">
+                    <span className="all">SEE ALL</span>
+                </div>
+            </div>
+            <div className="wrapper">
+                <div className="collections">
+                    <img className="imgtab" src="https://m.media-amazon.com/images/I/51z295C2UxL._UX210_FMjpg_QL85_.jpg"></img> 
+                    <div className="link-container">
+                        <a className="link" href="/albums/B0CHVTM66G?trackAsin=B0CHVSH3WS">JALSA 2.0 (From "Mission Raniganj: The Great Bharat Rescue")</a>
+                    </div> 
+                    <div className="content-container">
+                        <span className="content">Satinder Sartaaj &amp; Prem &amp; Hardeep</span>
+                    </div>
+                </div>
+                
+                <div className="collections">
+                    <img className="imgtab" src="https://m.media-amazon.com/images/I/51z295C2UxL._UX210_FMjpg_QL85_.jpg"></img> 
+                    <div className="link-container">
+                        <a className="link" href="/albums/B0CHVTM66G?trackAsin=B0CHVSH3WS">JALSA 2.0 (From "Mission Raniganj: The Great Bharat Rescue")</a>
+                    </div> 
+                    <div className="content-container">
+                        <span className="content">Satinder Sartaaj &amp; Prem &amp; Hardeep</span>
+                    </div>
+                </div>
+                
+            </div>
         </div>
     )
 }
+                    {/* {featuredSongs.map((song) => (
+                    <li key={song._id}>
+                        <img src={song.thumbnail} alt={song.title} />
+                        <p>{song.title}</p>
+                        <p>{song.artist.map((artist) => artist.name).join(', ')}</p>
+                    </li>
+                    ))} */}
 
+function SoulSoothers() {
+    return (
+        <div className="feature">
+            <div className="headertab">
+                <div className="header">
+                    <h2>Soul Soothers</h2>
+                </div>
+                <div className="options">
+                    <div>
+                        <ChevronCaretLeftIcon style={{ color: 'white' }}/>
+                    </div>
+                    <div>
+                        <ChevronCaretrightIcon style={{ color: 'white' }}/>
+                    </div>
+                </div>
+                <div className="alloptions">
+                    <span className="all">SEE ALL</span>
+                </div>
+            </div>
+            <div className="wrapper">
+                <div className="collections">
+                    <img className="imgtab" src="https://m.media-amazon.com/images/I/51z295C2UxL._UX210_FMjpg_QL85_.jpg"></img> 
+                    <div className="link-container">
+                        <a className="link" href="/albums/B0CHVTM66G?trackAsin=B0CHVSH3WS">JALSA 2.0 (From "Mission Raniganj: The Great Bharat Rescue")</a>
+                    </div> 
+                    <div className="content-container">
+                        <span className="content">Satinder Sartaaj &amp; Prem &amp; Hardeep</span>
+                    </div>
+                </div>
+                
+                <div className="collections">
+                    <img className="imgtab" src="https://m.media-amazon.com/images/I/51z295C2UxL._UX210_FMjpg_QL85_.jpg"></img> 
+                    <div className="link-container">
+                        <a className="link" href="/albums/B0CHVTM66G?trackAsin=B0CHVSH3WS">JALSA 2.0 (From "Mission Raniganj: The Great Bharat Rescue")</a>
+                    </div> 
+                    <div className="content-container">
+                        <span className="content">Satinder Sartaaj &amp; Prem &amp; Hardeep</span>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    )
+}
 export default Main;
