cce52f217defd11ba1685cde8b8598ca08c28b9e
Maheswari Kumar <ksrimahi@gmail.com>
2023-09-21 22:06:58 +0530

    Done

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,14 +6,15 @@ import ActionMoreIcon from "./ActionMoreIcon";
 import ActionAddIcon from "./ActionAddIcon";
 
 function Main(){
-    const [featuredata, setFeaturedata] = useState([]);
+    let [featuredata, setFeaturedata] = useState([]);
     const [selectleft, setSelectLeft] = useState("rgba(255, 255, 255, 0.3)");
     const [selectright, setSelectRight] = useState("white");
+    const [scrollLeft, setScrollLeft] = useState(0);
 
     useEffect(()=>{
         async function fetchFeaturedMusic() {
             try {
-                const response = await fetch('https://academics.newtonschool.co/api/v1/music/', {
+                const response = await fetch('https://academics.newtonschool.co/api/v1/music/album?limit=12', {
                     headers: {
                         'projectId': 'f104bi07c490'
                     }
@@ -27,7 +28,7 @@ function Main(){
         
                
                 console.log(data);
-                setFeaturedata(data);
+                setFeaturedata(data.data);
                 console.log(data.data);
                 console.log(data.data[0]);
 
@@ -49,25 +50,36 @@ function Main(){
     function handleRightIcon() {
         setSelectRight("white");
         setSelectLeft("rgba(255, 255, 255, 0.3)");
+        const scrollToX = 300; // Adjust this value as needed
+
+        // Use window.scrollTo to scroll to the specified position on the X-axis
+        window.scrollTo({
+          left: scrollToX,
+          behavior: 'smooth', // Use 'auto' for instant scrolling
+        });
+    
+        // Update the state to reflect the new scroll position
+        setScrollLeft(scrollToX);
     }
 
     return (
         <div className="Main-section">
             <div className="categories"></div>
-            <FeaturedMusic handleLeftIcon={handleLeftIcon} handleRightIcon={handleRightIcon} selectleft={selectleft} selectright={selectright}/>
+            <TrendingPlayLists featuredata={featuredata} handleLeftIcon={handleLeftIcon} handleRightIcon={handleRightIcon} selectleft={selectleft} selectright={selectright}/>
             <SoulSoothers />
             <WorkoutMix />
+            <TrendingSongs />
         </div>
 
     )
 }
 
-function FeaturedMusic({handleLeftIcon, handleRightIcon, selectleft, selectright}){
+function TrendingPlayLists({featuredata, handleLeftIcon, handleRightIcon, selectleft, selectright}){
     return (
         <div className="feature">
             <div className="headertab">
                 <div className="header">
-                    <h2>Featured This Week</h2>
+                    <h2>Trending Playlists</h2>
                 </div>
                 <div className="options">
                     <div onClick={handleLeftIcon}>
@@ -82,9 +94,10 @@ function FeaturedMusic({handleLeftIcon, handleRightIcon, selectleft, selectright
                 </div>
             </div>
             <div className="wrapper">
-                <div className="collections">
+                {featuredata.map((song, idx)=>(
+                <div className="collections" key={idx}>
                     <div className="image-container">
-                    <img className="imgtab" src="https://m.media-amazon.com/images/I/51z295C2UxL._UX210_FMjpg_QL85_.jpg"></img>
+                    <img className="imgtab" src={song.image} alt={song.title}></img>
                     <div className="icon-container">
                         <ActionAddIcon />
                         <div className="play-container">
@@ -94,21 +107,13 @@ function FeaturedMusic({handleLeftIcon, handleRightIcon, selectleft, selectright
                     </div> 
                     </div>
                     <div className="link-container">
-                        <a className="link" href="/albums/B0CHVTM66G?trackAsin=B0CHVSH3WS">JALSA 2.0 (From "Mission Raniganj: The Great Bharat Rescue")</a>
+                        <span className="link">{song.title}</span>
                     </div> 
                     <div className="content-container">
-                        <span className="content">Satinder Sartaaj &amp; Prem &amp; Hardeep</span>
+                        <span className="content">{song.artists[0].name}</span>
                     </div>
                 </div>
-                <div className="collections">
-                    <img className="imgtab" src="https://m.media-amazon.com/images/I/51z295C2UxL._UX210_FMjpg_QL85_.jpg"></img> 
-                    <div className="link-container">
-                        <a className="link" href="/albums/B0CHVTM66G?trackAsin=B0CHVSH3WS">JALSA 2.0 (From "Mission Raniganj: The Great Bharat Rescue")</a>
-                    </div> 
-                    <div className="content-container">
-                        <span className="content">Satinder Sartaaj &amp; Prem &amp; Hardeep</span>
-                    </div>
-                </div>
+                ))}
             </div>
         </div>
     )
@@ -204,6 +209,46 @@ function WorkoutMix() {
                 
             </div>
         </div>
+    )
+}
+
+function TrendingSongs() {
+    return (
+        <div className="feature">
+            <div className="headertab">
+                <div className="header">
+                    <h2>Workout Mix</h2>
+                </div>
+                <div className="options">
+                    <ChevronCaretLeftIcon style={{ color: 'white' }}/>
+                    <ChevronCaretrightIcon style={{ color: 'white' }}/>
+                </div>
+                <div className="alloptions">
+                    <span className="all">SEE ALL</span>
+                </div>
+            </div>
+            <div className="wrapper">
+                <div className="collections">
+                    <img className="imgtab" src="https://m.media-amazon.com/images/I/51z295C2UxL._UX210_FMjpg_QL85_.jpg"></img> 
+                    <div className="link-container">
+                        <a className="link" href="/albums/B0CHVTM66G?trackAsin=B0CHVSH3WS">JALSA 2.0 (From "Mission Raniganj: The Great Bharat Rescue")</a>
+                    </div> 
+                    <div className="content-container">
+                        <span className="content">Satinder Sartaaj &amp; Prem &amp; Hardeep</span>
+                    </div>
+                </div>
+                
+                <div className="collections">
+                    <img className="imgtab" src="https://m.media-amazon.com/images/I/51z295C2UxL._UX210_FMjpg_QL85_.jpg"></img> 
+                    <div className="link-container">
+                        <a className="link" href="/albums/B0CHVTM66G?trackAsin=B0CHVSH3WS">JALSA 2.0 (From "Mission Raniganj: The Great Bharat Rescue")</a>
+                    </div> 
+                    <div className="content-container">
+                        <span className="content">Satinder Sartaaj &amp; Prem &amp; Hardeep</span>
+                    </div>
+                </div>
+            </div>
+        </div> 
     )
 }
 
