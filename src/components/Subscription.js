92fb79c9cc8acc454fd4921f678b65d8a79bb2a3
Maheswari Kumar <ksrimahi@gmail.com>
2023-10-24 14:22:14 +0530

    Done

--- src/components/Subscription.js
+++ src/components/Subscription.js
@@ -8,36 +8,39 @@ function Subscription(){
                     <img className="logo" src="https://d5fx445wy2wpk.cloudfront.net/static/logo.svg"></img>
                     <nav>Pricing</nav>
                 </div>
-                <div>
-                    <nav></nav>
+                <div className="limit">
+                    <nav>UNLIMITED</nav>
                 </div>
-                <div>
-                    <div>
-                        <h1></h1>
-                        <nav></nav>
-                        <nav></nav>
-                        <nav></nav>
+                <div className="cards-sub">
+                    <div className="sub1">
+                        <h1>Individual</h1>
+                        <nav>non-Prime Members</nav>
+                        <nav>$9.99</nav>
+                        <nav>per month</nav>
                     </div>
-                    <div>
-                        <h1></h1>
-                        <nav></nav>
-                        <nav></nav>
-                        <nav></nav>
+                    <div className="sub2">
+                        <h1>Individual</h1>
+                        <nav>Prime Members</nav>
+                        <nav>$7.99</nav>
+                        <nav>per month</nav>
                     </div>
-                    <div>
-                        <h1></h1>
-                        <nav></nav>
-                        <nav></nav>
-                        <nav></nav>
+                    <div className="sub3">
+                        <h1>Family</h1>
+                        <nav>Prime Members</nav>
+                        <nav>$14.99</nav>
+                        <nav>per month</nav>
                     </div>
-                    <div>
-                        <h1></h1>
+                    <div className="sub4">
+                        <h1>Students</h1>
                         <nav></nav>
-                        <nav></nav>
-                        <nav></nav>
+                        <nav>$4.99</nav>
+                        <nav>per month</nav>
                     </div>
                 </div>
 
+            </div>
+            <div className="img-more">
+                <img src="https://d3dyfaf3iutrxo.cloudfront.net/file/editor/image/840d3c012f4847aba313ce82f354429c.png"></img>
             </div>
 
         </div>
